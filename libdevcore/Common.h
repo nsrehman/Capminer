354a4aa81c4afa99582ab45bfb5b21f172b70748
Michael Karimizadeh <50850314+michaelkari@users.noreply.github.com>
2022-03-24 18:46:34 -0400

    Rebrand to CapMiner
    Co-Authored-By: nsrehman <49407399+nsrehman@users.noreply.github.com>
    Co-Authored-By: Abdullahi-Ali903 <58126468+Abdullahi-Ali903@users.noreply.github.com>
    Co-Authored-By: Oliver Mo <56419482+omo-com@users.noreply.github.com>

--- libdevcore/Common.h
+++ libdevcore/Common.h
@@ -1,5 +1,5 @@
-// kawpowminer -- Ethereum miner with OpenCL, CUDA and stratum support.
-// Copyright 2018 kawpowminer Authors.
+// CapMiner -- Ethereum miner with OpenCL, CUDA and stratum support.
+// Copyright 2018 CapMiner Authors.
 // Licensed under GNU General Public License, Version 3. See the LICENSE file.
 
 /// @file
